3bf7b5a19d8e73467ed56340d2f4b10c0d5fd797
Nauzet Plasencia <nplasencia@auret.es>
2016-11-26 13:39:12 +0000

    Última prueba del algoritmo de cuadrantes

--- app/Http/Controllers/CuadrantesController.php
+++ app/Http/Controllers/CuadrantesController.php
@@ -181,6 +181,9 @@ class CuadrantesController extends Controller
 	    return $replacements;
     }
 
+	/**
+	 * El algoritmo no puede lanzarse ni un sábado ni un domingo
+	 */
     public function complexAlgorithm()
     {
         $servicesConditions = $this->getConditions();
@@ -191,7 +194,7 @@ class CuadrantesController extends Controller
 
 	    //Eliminamos todos aquellos servicios almacenados en la base de datos cuya fecha sea mayor a hoy
 	    $eliminarAPartirDe = new Carbon();
-		//$eliminarAPartirDe->setDate(2016, 11, 20);
+		$eliminarAPartirDe->setDate(2016, 11, 20);
 		$this->cuadranteRepository->deleteAllAfterDate($eliminarAPartirDe);
 
 	    for ($i=0; $i<8;$i++) {
@@ -211,7 +214,7 @@ class CuadrantesController extends Controller
 
 				    $now = new Carbon();
 				    $now->setTime(0, 0, 0);
-					//$now->setDate(2016, 11, 21);
+					$now->setDate(2016, 11, 21);
 				    $now->addWeeks( $i );
 				    if ($period == 4) continue;
 				    foreach ( $weekdays[ $period ] as $weekday ) {
@@ -366,8 +369,7 @@ class CuadrantesController extends Controller
 			    if ($date->dayOfWeek == Carbon::SUNDAY) {
 				    //No podemos asignar como replacement para el domingo al conductor que haya realizado el servicio 13 el sábado
 				    //TODO: Esto tengo que arreglarlo sí o sí
-				    $conductorServicio13Sabado = $cuadranteFinal[ $date->addDays(-1)->toDateString() ][ 75 ];
-				    $date->addDay();
+				    $conductorServicio13Sabado = $cuadranteFinal[ $date->copy()->addDays(-1)->toDateString() ][ 75 ];
 				    $drivers[] = $conductorServicio13Sabado->id;
 
 				    // Tampoco podemos asignar como replacements a los 5 conductores que seleccionemos para los servicios 28, 29, 30, 33 y 34 del domingo

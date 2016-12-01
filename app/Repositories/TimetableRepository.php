a54335f18e5bf691a7360f4ce3ee974f5483d120
Nauzet Plasencia <nplasencia@auret.es>
2016-12-01 00:20:12 +0000

    Faltaba cambiar el query ajax de tiemtables para que respetara la condición de mostrar aquellos services_timetbales cuyos deleted_at fueran null

--- app/Repositories/TimetableRepository.php
+++ app/Repositories/TimetableRepository.php
@@ -16,7 +16,7 @@ class TimetableRepository extends BaseRepository
     public function getByRouteNoService($route_id, $period_id)
     {
         $query = "SELECT * FROM cuadrantes.timetables   
-                    WHERE route_id=$route_id AND period_id=$period_id AND id NOT IN (SELECT timetable_id FROM service_timetables) 
+                    WHERE route_id=$route_id AND period_id=$period_id AND id NOT IN (SELECT timetable_id FROM service_timetables WHERE deleted_at is null) 
                     ORDER BY time";
         return $this->getEntity()->hydrateRaw($query);
     }

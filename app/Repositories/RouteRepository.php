950aec069aba8b7906c61a2ddd2d2bdbc398548b
Nauzet Plasencia <nplasencia@auret.es>
2016-12-01 00:03:40 +0000

    No se veían los horarios de aquellos servicios que habían sido eliminados

--- app/Repositories/RouteRepository.php
+++ app/Repositories/RouteRepository.php
@@ -22,7 +22,7 @@ class RouteRepository extends BaseRepository{
         $query = "SELECT ro.* FROM cuadrantes.timetables tim 
                     LEFT JOIN cuadrantes.routes ro ON tim.route_id=ro.id 
                     LEFT JOIN cuadrantes.lines li ON ro.line_id = li.id 
-                    WHERE tim.period_id=$period_id AND tim.id NOT IN (SELECT timetable_id FROM cuadrantes.service_timetables) 
+                    WHERE tim.period_id=$period_id AND tim.id NOT IN (SELECT timetable_id FROM cuadrantes.service_timetables WHERE deleted_at is not null) 
                     GROUP BY ro.id ORDER BY li.number ";
         return $this->getEntity()->hydrateRaw($query);
     }
